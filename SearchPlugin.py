4dd9628d575a67060b009ddeea38c9d07b028318
Alex Schoof <alex.schoof@gmail.com>
2010-05-12 21:03:54 -0400

    created skeleton for search plugins

--- SearchPlugin.py
+++ SearchPlugin.py
@@ -2,5 +2,15 @@ class SearchPlugin:
     """ Abstract class for search plugins. Child classes should implement it's methods to implement the API. """
 
     def __init__(self):
+        """ Create an instance of the searcher. """
         pass
 
+    def searchForShow(self, showName, seasonNumber, episodeNumber):
+        """ Search for a show. Results should be stored in some kind of internal data structure """
+        pass
+
+    def getResultList(self):
+        """ Get a list of the search results. Each result should contain data about the results and a link to the nzb file """
+        pass
+
+    
